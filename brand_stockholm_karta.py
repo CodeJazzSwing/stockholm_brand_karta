2832dcc14f61e12d15cf25a3ba71e2b80a3c7649
Noel <ramipani@yahoo.com>
2023-12-14 10:36:42 +0100

    Edit unnecessary code

--- brand_stockholm_karta.py
+++ brand_stockholm_karta.py
@@ -1,6 +1,5 @@
 #Dashboard with blue line around all datapoint.
-# Bad example since it does not hug around all the points.
-
+# Bad example since ConvexHull does not hug around all the points.
 
 import pandas as pd
 import streamlit as st
@@ -11,9 +10,9 @@ import numpy as np
 import plotly.graph_objects as go
 import matplotlib.ticker as ticker
 
-df = pd.read_csv(r'C:\\Users\\SE1D2L\\Documents\\Python Scripts\\Toy2.csv', sep=';')
+df = pd.read_csv(r'YOUR_PATH_FILE', sep=';')
 
-
+#Stockholm stad logo
 image = 'StockholmsStad_logotypeStandardA3_300ppi_svart.png'
 
 # Convert 'Tidpunkt_' to datetime
@@ -32,7 +31,7 @@ if start_date > end_date:
     st.sidebar.error('Misstag: Slutdatum måste vara efter startdatum.')
 
 
-# Filter the dataframe based on the sidebar selection
+# Filter the DF based on the sidebar selection
 df_selection = df[(df['Tidpunkt_'].dt.date >= start_date) & (df['Tidpunkt_'].dt.date <= end_date)]
 
 
@@ -45,19 +44,11 @@ col1.image(image, use_column_width=True)
 
 #Create color dictionary för column "Typ of brand" categories
 color_dict = {"Brand i byggnad" : "red", "Brand i container" : "blue", "Fordonsbrand" : "green", "Mark-/skogsbrand" : "orange", "Övrigt" : "purple"}
-
-
 categories = df_selection['Typ av brand'].unique()
 colors = [color_dict[cat] for cat in categories]
 
 
-#Adds a new column "color" to the  DF based on "Typ of brand" column
-
-# DO I NEED THIS LINE OF CODE?! Seems like not.
-df_selection["color"] = df_selection["Typ av brand"].map(color_dict)
-
-
-# Use 'Typ av brand' for color parameter in the plot and set color_discrete_sequence to your list of colors
+# Use 'Typ av brand' for color parameter in the plot and set color_discrete_sequence to colors
 fig = px.scatter_mapbox(df_selection,
                         lon = df_selection['lng'],
                         lat = df_selection['lat'],
@@ -73,30 +64,25 @@ fig = px.scatter_mapbox(df_selection,
 
 
 fig.update_traces(marker=dict(size = 12))
-
 fig.update_layout(mapbox_style='open-street-map')
 fig.update_layout(margin={'r':0,'t':50,'l':0,'b':10})
 
 
-# Display the plot in Streamlit
+# Display the plot using Streamlit
 st.plotly_chart(fig)
 
 
-#LINE GRAPH-----------------------
-import numpy as np
-import pandas as pd
-
-# Assuming 'Tidpunkt_' is the time and 'Typ av brand' is the type of fire
+#_______________________LINE GRAPH___________________________
 df_selection_grouped = df_selection.groupby(['Tidpunkt_', 'Typ av brand']).size().reset_index(name='counts')
 
-# Get unique fire types and time points
+# Get fire types and time points
 fire_types = df_selection_grouped['Typ av brand'].unique()
 time_points = df_selection_grouped['Tidpunkt_'].unique()
 
-# Create a dataframe with all combinations of time points and fire types
+# Create a DF with all combinations of time and fire
 df_all_combinations = pd.DataFrame(index=pd.MultiIndex.from_product([time_points, fire_types], names=['Tidpunkt_', 'Typ av brand'])).reset_index()
 
-# Merge the original dataframe with the dataframe that has all combinations
+# Merge the original DF with the DF that has all combinations
 df_merged = pd.merge(df_all_combinations, df_selection_grouped, on=['Tidpunkt_', 'Typ av brand'], how='left')
 
 # Fill NA values with 0
@@ -139,7 +125,7 @@ ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 # Display the plot
 st.pyplot(fig)
 
-#-----------Create a line around the map--------------
+#______________Create a line around the map________________OBS! This is a test.
 
 # Create the scatter mapbox for the points
 fig = go.Figure(go.Scattermapbox(
